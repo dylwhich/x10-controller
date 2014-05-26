cfcde3af0f0cc7fca5e34d7ac26dc5dce83e80b9
Alexander Bauer <sasha@crofter.org>
2014-05-26 19:17:02 -0400

    Make a number of testing changes to x10d

--- x10d.py
+++ x10d.py
@@ -42,25 +42,57 @@ class Daemon:
         conn = serial.Serial(port=self.serialpath)
 
         # Query the serial port for a "ready" signal.
+        # TODO: replace with control signal
         conn.write("READY?")
 
         # Continuously retrieve from the queue and send them to the
         # serial port. Prioritized signals are sent first.
         while True:
             # Wait until the serial port hears a request for more data.
-            
+            conn.read()
+
+            # Once ready, get a signal from the connection and
+            # serialize it over.            
+            conn.write(self.signals.get().serialize())
+            self.signals.task_done()
         
+    # Daemonize by listening for signals on the fifo and consume them
+    # from the priority queue simultaneously.
+    def start(self):
+        self.listener = threading.Thread()
+        self.listener.run = self.listenfifo
+
+        self.consumer = threading.Thread()
+        self.consumer.run = self.consume
+
+        self.listener.start()
+        self.consumer.start()
+
+    # Wait for daemonized threads to finish.
+    def join(self):
+        self.listener.join()
+        self.consumer.join()
 
 def log(string):
     print(string)
 
 def main(args):
-    pass
+    daemon = Daemon(fifo=args.fifo,
+                    statedb=args.statedb,
+                    serial=args.serial)
+    daemon.start()
+    daemon.join()
 
 def parse(argv):
     parser = argparse.ArgumentParser("Queue and manage x10 signals via x10lib")
     parser.add_argument("--fifo", "-f", default="/etc/x10ctl",
                         help="FIFO to read signals from")
+    parser.add_argument("--statedb", default="/etc/x10state.db",
+                        help="Database to store unit states in")
+
+    parser.add_argument("--serial", default="/dev/ttyACM0",
+                        help="Serial port to use")
+
     return parser.parse_args(argv)
 
 if __name__ == "__main__":
